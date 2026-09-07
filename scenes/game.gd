6736d1fdfe2d068bc3a3fabde0de93472d43bb42
Maya Miyazawa <majaa.mikhailovna@gmail.com>
2026-09-07 10:58:27 +0200

    Dialogue handling start

--- scenes/game.gd
+++ scenes/game.gd
@@ -1,11 +1,35 @@
 extends Node2D
+class_name DialogueHandler
 
-
+static var Instance: DialogueHandler = null
+signal next_line_prompted
+var reading_dialogue: bool
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	if Instance == null:
+		Instance = self
+	else:
+		queue_free()
+		return
+	reading_dialogue = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _input(event: InputEvent) -> void:
+	if not reading_dialogue: return
+	if event.is_action_pressed("interact"):
+		next_line_prompted.emit()
+	
+
+func load_dialogue(dialogue: Array[String]) -> void:
+	reading_dialogue = true
+	for text in dialogue:
+		if text:
+			print(text)
+		await next_line_prompted
+	reading_dialogue = false
+	
+		
--- scripts/click.gd
+++ scripts/click.gd
@@ -1,22 +1,24 @@
 extends Area2D
 
+# IMPORTANT! Always leave array element 0 of flavor_text empty.
 @export var flavor_text: Array[String]
-var hovered: bool
 # Called when the node enters the scene tree for the first time.
 	
 func _ready() -> void:
-	hovered = false
+	set_process_input(false)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
 func _input(event: InputEvent) -> void:
-	if not hovered: return
+	if DialogueHandler.Instance.reading_dialogue: return
 	if event.is_action_pressed("interact"):
-		print("A click!")
+		DialogueHandler.Instance.load_dialogue(flavor_text)
 			
 func _mouse_enter() -> void:
-	hovered = true
+	if DialogueHandler.Instance.reading_dialogue: return
+	set_process_input(true)
 func _mouse_exit() -> void:
-	hovered = false
+	if DialogueHandler.Instance.reading_dialogue: return
+	set_process_input(false)
