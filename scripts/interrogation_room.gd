0c7a839c1cb1d36039ab9a9256212cdd040228b5
G-insertfillertext <gigapq@gmail.com>
2026-09-11 21:31:33 +0200

    next

--- scripts/interrogation_room.gd
+++ scripts/interrogation_room.gd
@@ -4,11 +4,14 @@ var lady_lemon = FileAccess.open("res://dialogues/lady_lemon.json", FileAccess.R
 var mr_chili = FileAccess.open("res://dialogues/mr_chili.json", FileAccess.READ)
 var mr_onion = FileAccess.open("res://dialogues/mr_onion.json", FileAccess.READ)
 
+var dialogue_list : Array = [lady_lemon, mr_chili, mr_onion]
 var current_dialogue = JSON.parse_string(lady_lemon.get_as_text())
+var dialogue_counter : int = 0
 
 var choice : bool = false
 var canInteract : bool = false
 var line_counter : int = 0
+var previous_was_text : bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -24,19 +27,38 @@ func _process(delta: float) -> void:
 		canInteract = true
 	else:
 		canInteract = false
+	 
+	#print(canInteract)
 
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("interact") and canInteract:
-		next_line()
-
+		if current_dialogue[line_counter]["end"] == false:
+			next_line()
+		else:
+			$NextLayer.show()
+			await $NextLayer/Button.pressed
+			$NextLayer.hide()
+			for profile in current_dialogue[line_counter]["end_info"].keys():
+				Notebook.add_profile_info(profile, current_dialogue[line_counter]["end_info"][profile])
+			dialogue_counter += 1
+			current_dialogue = JSON.parse_string(dialogue_list[dialogue_counter].get_as_text())
+			line_counter = 0
+			DialogueHandler.load_dialogue([current_dialogue[line_counter]["text"]])
 
 func next_line() -> void:
 	line_counter += 1
 	if current_dialogue[line_counter]["type"] == "text":
 		choice = false
+		canInteract = false
 		$ButtonsLayer.hide()
-		DialogueHandler.load_dialogue([current_dialogue[line_counter]["text"]])
+		print(current_dialogue[line_counter]["text"])
+		if previous_was_text:
+			await DialogueHandler.done
+			DialogueHandler.load_dialogue([current_dialogue[line_counter]["text"]])
+		else:
+			DialogueHandler.load_dialogue([current_dialogue[line_counter]["text"]])
+		previous_was_text = true
 	elif current_dialogue[line_counter]["type"] == "choice":
 		choice = true
 		$ButtonsLayer/Button1.text = current_dialogue[line_counter]["1"]
